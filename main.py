5c0bbfc08b7220ec4bcae3cc827ef1c5cbb6ae88
meglamp <megan.lampley@students.fhu.edu>
2024-02-01 10:24:04 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,10 @@
 #REMOVE PASS AND FIX THE FUNCTION
 #change to test push and forks
 def sum_of_products(list1, list2):
-    lst1 = []
-    lst2 = []
     num = 0
-    for letter in list1:
-        lst1.append(letter)
-    for letter in list2:
-        lst2.append(letter)
 
     for i in range(0,len(list1)):
-        num = num + (int(lst1[i])*int(lst2[i]))
+        num = num + (int(list1[i])*int(list2[i]))
     return(num)
         
 
@@ -20,6 +14,5 @@ if len(lis1) == len(lis2):
     number = sum_of_products(lis1,lis2)
     print(number)
 else:
-    print("error")
-    
+    print("Error: numbers must be the same length")
 
