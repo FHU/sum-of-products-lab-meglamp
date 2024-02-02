7f529d86b19d746d79c0b799ff8d4fda12132ba7
meglamp <megan.lampley@students.fhu.edu>
2024-02-01 20:45:45 -0600

    Update main.py
    Another Another Try

--- main.py
+++ main.py
@@ -1,13 +1,22 @@
 #REMOVE PASS AND FIX THE FUNCTION
 #change to test push and forks
 def sum_of_products(list1, list2):
-    num = 0
+    total = 0
+    li1 = int(list1)
+    li2 = int(list2)
 
     for i in range(0,len(list1)):
-        par1 = int(list1[i])
-        par2 = int(list2[i])
-        num = num + (par1*par2)
-    return(num)
+        if i == 0:
+            listy1 = li1 % 10
+            listy2 = li2 % 10 
+        else:
+            li1 = li1 // 10
+            li2 = li2 //10
+            listy1 = li1 % 10
+            listy2 = li2 % 10
+        numby = listy1 * listy2
+        total = total + numby
+    return(total)
         
 
 if __name__ == '__main__':
